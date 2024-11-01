2c2e1bc749ae89fbd748786a6153d77845d26fc4
qyvlik <qyvlik@qq.com>
2024-11-01 21:19:51 +0800

    feat: add

--- jdph-volume/src/main/java/io/github/qyvlik/jdph/examples/JDPHVolumeMain.java
+++ jdph-volume/src/main/java/io/github/qyvlik/jdph/examples/JDPHVolumeMain.java
@@ -5,13 +5,20 @@ import io.github.qyvlik.jdph.examples.coffer.id.CredentialManager;
 import io.github.qyvlik.jdph.examples.coffer.id.env.MemoryCredentialManager;
 import io.github.qyvlik.jdph.plugins.volume.Server;
 import io.github.qyvlik.jdph.vertx.VertxWebContainer;
-
+import java.io.PrintStream;
 import java.io.IOException;
 import java.net.UnixDomainSocketAddress;
 import java.util.Map;
 
 public class JDPHVolumeMain {
     public static void main(String[] args) throws IOException {
+
+        String logFile = System.getenv("LOG_FILE");
+        if (logFile != null && !logFile.isBlank()) {
+            var ps = new PrintStream(logFile);
+            System.setOut(ps);
+            System.setErr(ps);
+        }
 
         Map<String, String> envs = System.getenv();
         CredentialManager cm = MemoryCredentialManager.create(envs, MemoryCredentialManager.CREDENTIAL_PREFIX);
